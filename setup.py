232f71c7a7a22107515a89839c2af13017fe7460
Chris Garrett <christopher.garrett@austin.utexas.edu>
2023-10-02 14:08:06 -0500

    refactor

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name="after-black",
+    name="after_black",
     description="To help idenitfy possible formatting issues after running the Black formatter.",
-    url="https://github.austin.utexas.edu/cmg5997/after-black",
-    version="0.0.1",
+    url="https://github.com/cgarrett/after-black",
+    version="v0.0.1",
 
     author="Chris Garrett",
     author_email="christopher.garrett@austin.utexas.edu",
@@ -17,7 +17,7 @@ setup(
     packages=find_packages("./src"),
     entry_points={
         "console_scripts": [
-            "after-black=src.after-black:main",
+            "after_black=src.after_black:main",
         ],
     },
 )
