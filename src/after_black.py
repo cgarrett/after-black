c0fbcfb90f4da58124976f97fd19adc174aaf5d9
Chris Garrett <christopher.garrett@austin.utexas.edu>
2023-10-02 15:52:31 -0500

    Updates to remove some common false/positives

--- src/after_black.py
+++ src/after_black.py
@@ -27,6 +27,8 @@ def main(argv: Sequence[str] | None = None) -> int:
         "' \"",
     ]
 
+    potential_issues = 0
+
     for filename in args.filenames:
         with open(filename, "r") as file:
             lines = file.readlines()
@@ -34,8 +36,22 @@ def main(argv: Sequence[str] | None = None) -> int:
             for line_no, line in enumerate(lines):
                 for test in test_list:
                     if test in line:
-                        print(f"{filename} - Line {line_no}: {line}")
-                        retval = 1
+                        position = line.index(test)
+
+                        if (
+                            # for blah = " " + blah situations
+                            line[position+3:].lstrip().startswith("+") or
+                            # for replace("", " ") situations
+                            line[position+3:].lstrip().startswith(")") or
+                            # for replace(" ", "_") situations
+                            line[position-1] == "("
+                        ):
+                            pass
+                        else:
+                            potential_issues += 1
+                            print(f"{filename} - Line {line_no} (pos: {position}): {line.lstrip()}".rstrip())
+                            retval = 1
+
     return retval
 
 
