998b7e1a94455fbe1bab98ab8d5d0fc81976ec2f
Chris Garrett <christopher.garrett@austin.utexas.edu>
2023-10-02 14:26:35 -0500

    moved setup settings

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-from setuptools import find_packages
 from setuptools import setup
 
-setup(
-    name="after_black",
-    description="To help idenitfy possible formatting issues after running the Black formatter.",
-    url="https://github.com/cgarrett/after-black",
-    version="v0.0.1",
-
-    author="Chris Garrett",
-    author_email="christopher.garrett@austin.utexas.edu",
-
-    classifiers=[
-        'Programming Language :: Python :: 3',
-    ],
-
-    packages=find_packages("./src"),
-    entry_points={
-        "console_scripts": [
-            "after_black=src.after_black:main",
-        ],
-    },
-)
+setup()
